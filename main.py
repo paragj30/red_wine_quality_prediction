e76a9df56de59c8c508cef40025b4d3c379e013f
unknown <paragj30@gmail.com>
2024-05-10 23:40:13 +0200

    data transformation pipeline added

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from src.mlproject import logger
 from src.mlproject.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlproject.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlproject.pipelines.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion_obj = DataIngestionTrainingPipeline()
+    data_ingestion_obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
 
 
+
 STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataValidationTrainingPipeline()
-    obj.main()
+    data_validation_obj = DataValidationTrainingPipeline()
+    data_validation_obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transform_obj = DataTransformationTrainingPipeline()
+    data_transform_obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/MLProject/config/configuration.py
+++ src/MLProject/config/configuration.py
@@ -1,7 +1,7 @@
 
 from src.mlproject.constant import *
 from src.mlproject.utils.common import read_yaml, create_directories
-from src.mlproject.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from src.mlproject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -46,3 +46,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
