e044df1529cd34cc21e1884cee49bb3f46b20ba9
paragj30 <paragj30@gmail.com>
2024-05-17 09:27:58 +0200

    hyparameterwith model training file added

--- src/mlproject/constant/__init__.py
+++ src/mlproject/constant/__init__.py
@@ -3,3 +3,4 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+ML_MODEL_FILE_PATH = Path("ml_model.yaml")
--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -3,14 +3,13 @@ import pandas as pd
 import numpy as np
 from box.exceptions import BoxValueError
 from yaml import safe_load
-from src.mlproject import logger
+from mlproject import logger
 import json
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import GridSearchCV
-import joblib
-from ensure import ensure_annotations 
+import joblib 
 import sys
-from src.mlproject.exception import CustomException
+from mlproject.exception import CustomException
 
 '''
 ensure_annotations: ensure that whatever arguments are of the same type are passed to the constructor.
@@ -26,7 +25,7 @@ from pathlib import Path
 from typing import Any 
 
 
-@ensure_annotations
+
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -51,7 +50,26 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         CustomException(e, sys)
 
 
-@ensure_annotations
+
+def read_ml_model_yaml(path_to_yaml: Path):
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = safe_load(yaml_file)
+            logger.info(f"ml_model.yaml file: {path_to_yaml} loaded successfully")
+            return(content)
+    except Exception as e:
+        CustomException(e, sys)
+
+
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
@@ -65,7 +83,6 @@ def create_directories(path_to_directories: list, verbose=True):
             logger.info(f"created directory at: {path}")
 
 
-@ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
 
@@ -80,8 +97,6 @@ def save_json(path: Path, data: dict):
 
 
 
-
-@ensure_annotations
 def load_json(path: Path) -> ConfigBox:
     """load json files data
 
@@ -99,66 +114,6 @@ def load_json(path: Path) -> ConfigBox:
 
 
 
-@ensure_annotations
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-
-    """Evaluate models using GridSearchCV
-
-    Args:
-        path (Path): path to json file
-        X_train: X_train object
-        y_train: X_train object
-        X_test: X_test object
-        y_test: y_test object
-        models: list of models
-        param: dict of hyperparameters of the model.
-
-    Returns:
-        r2_score, MAE, RSME: Evaluation metrics of the model.
-    """
-    try:
-        report_r2 = {}
-        report_mae = {}
-        report_rmse = {}
-
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model=model,
-                            param_grid=para,
-                            cv=5,
-                            scoring='r2',
-                            error_score='raise',
-                            n_jobs=-1)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-
-            train_r2_score = r2_score(y_train, y_train_pred)
-        
-            test_r2_score = r2_score(y_test, y_test_pred)
-            test_mae = mean_absolute_error(y_test, y_test_pred)
-            test_mse = mean_squared_error(y_test, y_test_pred)
-            test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
-
-            report_r2[list(models.keys())[i]] = test_r2_score
-            report_mae[list(models.keys())[i]] = test_mae
-            report_rmse[list(models.keys())[i]] = test_rmse
-
-        return report_r2, report_mae, report_rmse
-    except BoxValueError:
-        raise ValueError("Issue with the model training and evaluation part in common.py file")
-    except Exception as e:
-        CustomException(e, sys)
-
-
-
-@ensure_annotations
 def save_bin(data: Any, path: Path):
     """save binary file
 
@@ -170,7 +125,7 @@ def save_bin(data: Any, path: Path):
     logger.info(f"binary file saved at: {path}")
 
 
-@ensure_annotations
+
 def load_bin(path: Path) -> Any:
     """load binary data
 
@@ -186,7 +141,7 @@ def load_bin(path: Path) -> Any:
 
 
 
-@ensure_annotations
+
 def get_size(path: Path) -> str:
     """get size in KB
 
